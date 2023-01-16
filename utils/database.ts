bc66ebc6af44940ff3063e4b8d627fe71a75eeb3
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-01-16 21:02:00 +0800

    feat(utilities): :bug: wrap connect mongoose in callback

--- utils/database.ts
+++ utils/database.ts
@@ -6,6 +6,14 @@ if (typeof NUXT_MONGO_URL === "undefined") {
   throw new Error("NUXT_MONGO_URL is not defined in the .env file");
 }
 
-mongoose.connect(NUXT_MONGO_URL, {
-  dbName: "lms",
-});
+export const connectToDB = () => {
+  return mongoose.connect(
+    NUXT_MONGO_URL,
+    {
+      dbName: "lms",
+    },
+    () => {
+      console.log("connected to database");
+    }
+  );
+};
