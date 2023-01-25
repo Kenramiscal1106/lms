e8e401c1777552b034adaad5a958bc060b288be9
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-01-25 20:33:37 +0800

    feat(api): :sparkles: add api logic

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -1,6 +1,31 @@
+import bcrypt from "bcrypt";
+
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
-  console.log(body);
+  const { username, password } = await readBody(event);
+
+  const user = await Users.findOne({
+    username,
+  });
+
+  if (!user) {
+    return {
+      success: false,
+    };
+  }
+
+  if (!user.passwordHash) {
+    return {
+      success: false,
+    };
+  }
+
+  const pwCompare = await bcrypt.compare(password, user.passwordHash);
+
+  if (!pwCompare) {
+    return {
+      success: false,
+    };
+  }
   return {
     success: "idk",
   };
