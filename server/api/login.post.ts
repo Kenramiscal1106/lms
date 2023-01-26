9b6c5b7ab90c622e4557710ff915383134bfe8a2
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-01-26 09:51:40 +0800

    feat(api): :sparkles: update logic
    updated logic of the api route (added error handling, success, cookie session)

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -1,4 +1,6 @@
 import bcrypt from "bcrypt";
+import { Users } from "@/utils/models";
+import { v4 as uuidv4 } from "uuid";
 
 export default defineEventHandler(async (event) => {
   const { username, password } = await readBody(event);
@@ -8,25 +10,61 @@ export default defineEventHandler(async (event) => {
   });
 
   if (!user) {
-    return {
-      success: false,
-    };
+    throw createError({
+      statusCode: 400,
+      data: {
+        success: false,
+        message: "incorrect username and password",
+      },
+    });
   }
 
   if (!user.passwordHash) {
-    return {
-      success: false,
-    };
+    throw createError({
+      statusCode: 500,
+      data: {
+        success: false,
+        message: "password hash on database not present",
+      },
+    });
   }
-
   const pwCompare = await bcrypt.compare(password, user.passwordHash);
 
+  console.log(pwCompare);
   if (!pwCompare) {
-    return {
-      success: false,
-    };
+    throw createError({
+      statusCode: 400,
+      data: {
+        success: false,
+        message: "incorrect username or password",
+      },
+    });
   }
+  try {
+    user.userAuthToken = uuidv4();
+    user.save();
+    setCookie(event, "dbSession", user.userAuthToken, {
+      path: "/",
+      // server side only cookie so you can't use `document.cookie`
+      httpOnly: true,
+      // only requests from same site can send cookies
+      // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
+      sameSite: "strict",
+      // only sent over HTTPS in production
+      secure: process.env.NODE_ENV === "production",
+      // set cookie to expire after a month
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  } catch {
+    // setResponseStatus(500);
+    throw createError({
+      statusCode: 500,
+      message: "failed to update session",
+    });
+  }
+  // setResponseStatus(200, "logged in");
   return {
-    success: "idk",
+    success: true,
+    message: "successfully logged in",
   };
 });
