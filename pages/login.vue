c7ddcbae969335846402f599552510dc2cff3a85
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-01-26 09:50:14 +0800

    feat(pages): :sparkles: update login page
    added more features to the login (error handling, client side logic, styling, etc)

--- pages/login.vue
+++ pages/login.vue
@@ -1,33 +1,81 @@
 <script setup lang="ts">
+// import z from "zod"
+// definePageMeta({
+//   middleware: "auth"
+// })
+// const formResult = ref<{
+//   success: boolean,
+//   type: "incorrect" | "validation",
+//   message: string
+// } | null>(null)
 
 const handleSubmit = (async (e) => {
-  const formData = new FormData(e.currentTarget as HTMLFormElement)
+  const formTarget = e.currentTarget as HTMLFormElement
+  const formData = new FormData(formTarget)
   const username = formData.get("username")
   const password = formData.get('password')
+
+  const formSchema = z.object({
+    username: z.string().min(4),
+    password: z.string().min(8)
+  })
+
+  const parsedData = formSchema.safeParse({ username, password })
+
+  if (!parsedData.success) {
+    formResult.value = {
+      success: false,
+      type: "validation",
+      message: "validation error"
+    }
+    return
+  }
 
   const postReq = await fetch("/api/login", {
     method: "POST",
     body: JSON.stringify({ username, password })
   });
   const postRes = await postReq.json()
-  console.log({ username, password, postRes })
+  if (postReq.status === 400) {
+    formResult.value = {
+      success: false,
+      type: "incorrect",
+      message: "incorrect username or password"
+    }
+    return
+  }
+  if (postReq.status === 500) {
+    formResult.value = {
+      success: false,
+      type: "incorrect",
+      message: "internal server error"
+    }
+  }
+  navigateTo({
+    path: "/"
+  })
 }) satisfies EventListener
+useHead({
+  title: "Login | LMS"
+})
 </script>
 
 <template>
   <h1>Login to LMS</h1>
   <form @submit.prevent="handleSubmit">
-    <label for="username">Username</label>
+    <label for="username">Username</label><br>
     <input type="text" name="username" id="username"> <br>
-    <label for="password">Password</label>
+    <label for="password">Password</label><br>
     <input type="password" name="password" id="password"> <br>
     <button type="submit">Submit</button>
+    <div v-if="formResult !== null && !formResult.success">
+      {{ formResult.message }}</div>
   </form>
 </template>
 
 <style scoped>
 input {
   border: 1px solid black;
-  padding: .5rem;
+  padding: .25rem .5rem;
 }
 </style>
