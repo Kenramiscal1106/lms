de7442c6901bf2e50f4facac8aa6109e14af208a
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-02-05 21:18:37 +0800

    feat(pages): :sparkles: add middleware auth

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: "auth"
+})
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <h1>Empty page</h1>
   </div>
 </template>
