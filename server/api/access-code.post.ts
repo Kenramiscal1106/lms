c3a6832589ca129e744059cca8224817ab98015c
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-02-11 16:07:11 +0800

    feat(api): :sparkles: add api route for access code

---
+++ server/api/access-code.post.ts
@@ -0,0 +1,30 @@
+import { Users } from "@/utils/models";
+
+export default defineEventHandler(async (event) => {
+  /* const sessionCookie = await getCookie(event, "dbSession");
+
+  if (!sessionCookie) {
+    throw createError({
+      statusCode: 400,
+      message: "unauthorized",
+    });
+  }
+
+  const user = await Users.findOne({
+    userAuthToken: sessionCookie,
+  })
+    .populate("courses")
+    .select("courses");
+
+  console.log(user);
+  if (!user) {
+    throw createError({
+      statusCode: 400,
+      message: "session expired",
+    });
+  }
+
+  return {
+    courses: user.courses,
+  }; */
+});
