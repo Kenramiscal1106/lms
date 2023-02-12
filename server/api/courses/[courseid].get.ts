41272b6a13cfeb5fb24176fa47001a6ad888b258
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-02-12 14:41:57 +0800

    feat(api): :sparkles: add dynamic route for a course

---
+++ server/api/courses/[courseid].get.ts
@@ -0,0 +1,25 @@
+import { Users, Courses } from "@/utils/models";
+
+export default defineEventHandler(async (event) => {
+  const sessionCookie = getCookie(event, "dbSession");
+
+  if (!sessionCookie) {
+    throw createError({
+      statusCode: 400,
+      message: "unauthorized",
+    });
+  }
+
+  const targetCourse = await Courses.findById(event.context.params?.courseid, {
+    name: true,
+    members: true,
+  }).populate("members", {
+    firstName: true,
+    lastName: true,
+    username: true,
+  });
+
+  return {
+    courses: targetCourse,
+  };
+});
