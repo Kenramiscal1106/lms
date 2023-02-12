b33610226d3337e3a10fbcf6de2a017622928b0c
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-02-12 14:42:21 +0800

    refactor(pages): :truck: rename and add middleware

--- pages/courses/[coursename].vue
+++ pages/courses/[coursename].vue
@@ -1,11 +1,12 @@
 <script setup lang='ts'>
-const router = useRoute()
-console.log(router.params)
+const route = useRoute()
+console.log(route.params)
+definePageMeta({
+  middleware: ["auth"]
+})
 </script>
 <template>
-  <Navbar />
   <h1>Course page</h1>
-  {{ router.params }}
 </template>
 <style scoped>
 
