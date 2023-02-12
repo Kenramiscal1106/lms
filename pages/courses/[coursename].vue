5ea14b5a2b0b5eef40fa9fca4cd632a1fcd6556b
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-02-12 14:13:09 +0800

    feat(pages): :sparkles: add page for courses

---
+++ pages/courses/[coursename].vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+const router = useRoute()
+console.log(router.params)
+</script>
+<template>
+  <Navbar />
+  <h1>Course page</h1>
+  {{ router.params }}
+</template>
+<style scoped>
+
+</style>
