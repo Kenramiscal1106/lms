8a68cbbb38e64c0d6e308585d64b50522dd2b975
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-02-16 18:25:14 +0800

    feat(pages): :sparkles: add nested route for index and members

---
+++ pages/courses/[coursename]/index.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+const { data } = defineProps({
+  data: String
+})
+</script>
+<template>
+  <h1>Home</h1>
+  {{ data }}
+</template>
+<style scoped>
+
+</style>
---
+++ pages/courses/[coursename]/members.vue
@@ -0,0 +1,23 @@
+<script setup lang='ts'>
+const { data } = defineProps<{
+  data: any
+}>()
+</script>
+<template>
+  <h1>Members</h1>
+  <table>
+    <tbody>
+      <tr>
+        <th>name</th>
+        <th>username</th>
+      </tr>
+      <tr v-for="member in data.members">
+        <td>{{ member.firstName + " " + member.lastName }}</td>
+        <td>{{ member.username }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<style scoped>
+
+</style>
