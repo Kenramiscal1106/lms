908ae7cdd44d5d971be073ab93c6b4073a174188
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-02-16 18:25:57 +0800

    feat(pages): :sparkles: add fetch to api route add navigation links add nested layout and its props

--- pages/courses/[coursename].vue
+++ pages/courses/[coursename].vue
@@ -1,12 +1,20 @@
 <script setup lang='ts'>
 const route = useRoute()
+const { data } = await useFetch(`/api/courses/${route.params.coursename}`)
 console.log(route.params)
 definePageMeta({
   middleware: ["auth"]
 })
+useHead({
+  title: data.value.name
+})
 </script>
 <template>
-  <h1>Course page</h1>
+  <aside>
+    <NuxtLink :to="`/courses/${route.params.coursename}`">Materials</NuxtLink>
+    <NuxtLink :to="`/courses/${route.params.coursename}/members`">Members</NuxtLink>
+  </aside>
+  <NuxtPage :data="data" />
 </template>
 <style scoped>
 
