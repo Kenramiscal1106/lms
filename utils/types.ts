c9024a8aa545c90262dea8bdf23bfb0260a03fb2
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-02-16 20:15:00 +0800

    feat(utilities): :label: update types

--- utils/types.ts
+++ utils/types.ts
@@ -1,12 +1,23 @@
 import { Types } from "mongoose";
 
 export type CourseSchema = {
+  _id: Types.ObjectId;
   name: string;
   assignments: any[];
   quizes: any[];
   forums: any[];
   pages: any[];
   posts: any[];
-  members: Types.ObjectId;
+  members: Types.ObjectId[];
   role: "student" | "teacher" | "admin";
+};
+
+export type UserSchema = {
+  _id: Types.ObjectId;
+  firstName: string;
+  passwordHash: string;
+  userAuthToken: string;
+  lastName: string;
+  username: string;
+  courses: Types.ObjectId[];
 };
