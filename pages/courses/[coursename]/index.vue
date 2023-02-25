5cc7d0f412a9c5b2b10df096ed77e9d2d5e4ee55
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-02-26 04:44:07 +0800

    feat(pages): :sparkles: update page logic, used state management for data instead of props

--- pages/courses/[coursename]/index.vue
+++ pages/courses/[coursename]/index.vue
@@ -1,12 +1,57 @@
 <script setup lang='ts'>
-const { data } = defineProps({
-  data: String
+const courseStore = useCurrentCourse();
+if (!courseStore.value) {
+  throw createError({
+    statusCode: 400,
+    statusMessage: "Unresolved error"
+  })
+}
+const route = useRoute()
+console.log(courseStore.value)
+const { data } = await useFetch(`/api/courses/${route.params.coursename}/materials`)
+const userStore = useCurrentUser();
+console.log(data.value)
+const createModal = reactive<{
+  open: boolean,
+  type: "page" | "quizes" | "forums" | "assignments" | ""
+}>({
+  open: false,
+  type: ""
 })
+const openModalAction = (type: typeof createModal["type"]) => {
+  console.log(type)
+  createModal.open = true
+  createModal.type = type
+}
 </script>
 <template>
-  <h1>Home</h1>
-  {{ data }}
-</template>
-<style scoped>
+  <div v-if="courseStore">
 
-</style>
+    <h1>{{ courseStore.name }}</h1>
+    <div>
+      <h1>Assignments</h1>
+      <button v-if="userStore && userStore.userData && userStore.userData.role === 'teacher'"
+        @click="openModalAction('assignments')">Add Assignment</button>
+      <div v-if="courseStore.assignments.length !== 0">
+        <div v-for="assignment in courseStore.assignments">
+          {{ assignment.title }}
+          {{ assignment.assignmentBody }}
+        </div>
+      </div>
+    </div>
+    <div>
+      <h1>Pages</h1>
+      <button v-if="userStore && userStore.userData && userStore.userData.role === 'teacher'"
+        @click="openModalAction('page')">Add Page</button>
+      <div v-if="courseStore.assignments.length !== 0">
+        <div v-for="page in courseStore.pages">
+          {{ page.title }}
+          {{ page.assignmentBody }}
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- <template v-if="courseStore">
+                                        </template> -->
+</template>
+<style scoped></style>
