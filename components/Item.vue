fb2c90a5bceb159d5b5351297b544bc219b07a53
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-03-08 03:43:17 +0800

    feat(components): :sparkles: add recursive component for display of materials

---
+++ components/Item.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { FsItem } from '~~/utils/types';
+
+const { item } = defineProps<{
+  item: FsItem
+}>()
+</script>
+
+<template>
+  <li v-if="item.type === 'folder'">
+    {{ item.name }}
+    <ListItems :file-structure="item.children" />
+  </li>
+  <li v-else>
+    <NuxtLink :to="`${item.type}/${item.to}`">{{ item.name }}</NuxtLink>
+  </li>
+</template>
---
+++ components/ListItems.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+// import { defineProps } from 'vue';
+import type { FsItem } from "~~/utils/types"
+const { fileStructure } = defineProps<{
+  fileStructure: FsItem[]
+}>()
+
+</script>
+
+<template >
+  <ul v-for="file in fileStructure">
+    <Item :item="file" />
+  </ul>
+</template>
