432a571612483659adadce8cd9f07e9371e4a486
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-03 16:29:06 +0800

    feat(pages): :lipstick: update ui * updated links * updated styles and hover states

--- pages/course/[coursename].vue
+++ pages/course/[coursename].vue
@@ -23,11 +23,52 @@ useHead({
 })
 </script>
 <template>
-  <h1>{{ data && data.name }}</h1>
-  <aside>
-    <NuxtLink :to="`/course/${route.params.coursename}`">Materials</NuxtLink>
-    <NuxtLink :to="`/course/${route.params.coursename}/members`">Members</NuxtLink>
-  </aside>
-  <NuxtPage />
+  <main>
+    <aside>
+      <div class="link-container">
+        <NuxtLink :to="`/course/${route.params.coursename}`"
+          :class="`link ${route.path === `/course/${route.params.coursename}` ? ' open' : ''}`">Materials
+        </NuxtLink>
+        <NuxtLink :to="`/course/${route.params.coursename}/updates`"
+          :class="`link ${route.path === `/course/${route.params.coursename}/updates` ? ' open' : ''}`">Updates
+        </NuxtLink>
+        <NuxtLink :to="`/course/${route.params.coursename}/members`"
+          :class="`link ${route.path === `/course/${route.params.coursename}/members` ? ' open' : ''}`">Members
+        </NuxtLink>
+      </div>
+    </aside>
+    <div>
+
+      <h1>{{ data && data.name }}</h1>
+
+      <NuxtPage />
+    </div>
+  </main>
 </template>
-<style scoped></style>
+<style scoped>
+.link:not(.open) {
+  color: rgb(95, 95, 95);
+  text-decoration: none;
+}
+
+.link:hover {
+  color: black;
+}
+
+.open {
+  color: black;
+}
+
+.link-container {
+  display: flex;
+  flex-flow: column nowrap;
+}
+
+main {
+  display: flex;
+}
+
+aside {
+  width: 10rem;
+}
+</style>
