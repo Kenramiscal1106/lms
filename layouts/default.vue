f64a5b24cddfecc85217769be2c90f5adb9ec93c
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-16 06:42:43 +0800

    style(pages): :lipstick: add default styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,35 @@
 <template>
   <Navbar />
   <slot></slot>
-
 </template>
+
+<style>
+@tailwind base;
+
+@layer base {
+  * {
+    font-family: "Inter";
+  }
+
+  h1 {
+    @apply text-3xl;
+  }
+
+  h2 {
+    @apply text-2xl;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    @apply font-bold;
+  }
+
+  ul {
+    @apply list-disc;
+  }
+}
+</style>
