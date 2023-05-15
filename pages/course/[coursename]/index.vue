f785e1eaa442e62bf14248bf338c5c843644c804
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-16 06:44:06 +0800

    feat(pages): :sparkles: add menu

--- pages/course/[coursename]/index.vue
+++ pages/course/[coursename]/index.vue
@@ -7,33 +7,40 @@ if (typeof route.params.coursename !== "string") {
   })
 }
 
-const course = await useCourseData(route.params.coursename)
 const { data: materials } = await useFetch(`/api/course/${route.params.coursename}/materials`)
-const userStore = useCurrentUser();
-// console.log(data.value)
-const createModal = reactive<{
-  open: boolean,
-  type: "page" | "quizes" | "forums" | "assignments" | ""
-}>({
-  open: false,
-  type: ""
-})
-const openModalAction = (type: typeof createModal["type"]) => {
-  console.log(type)
-  createModal.open = true
-  createModal.type = type
-}
+const modalOpen = ref<boolean>(false);
 </script>
 <template>
   <div v-if="materials">
-
     <h2>materials</h2>
+    <button @click="modalOpen = true">Add Material</button>
     <div v-if="materials.folderStructure.length !== 0">
-
       <ListItems :fileStructure="materials.folderStructure" />
     </div>
     <div v-else>
       There are no materials
+    </div>
+    <div v-if="modalOpen"
+      class="bg-black bg-opacity-30 absolute w-screen h-screen top-0 left-0 flex justify-center items-center">
+      <div class="bg-white ">
+        <div class="max-w-4xl">
+          <div>
+            <button>Folder</button>
+          </div>
+          <div>
+            <NuxtLink :to="route.path + '/quiz-create'">Quiz</NuxtLink>
+          </div>
+          <div>
+            <NuxtLink :to="route.path + '/assignment-create'">Assignment</NuxtLink>
+          </div>
+          <div>
+            <NuxtLink :to="route.path + '/page-create'">Page</NuxtLink>
+          </div>
+          <div>
+            <NuxtLink :to="route.path + '/forum-create'">Forums</NuxtLink>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
