f5ed0de7b55e78ea9f3f0c497530a5e545391bb3
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-17 22:19:58 +0800

    feat(pages): :fire: remove links to creates

--- pages/course/[coursename]/index.vue
+++ pages/course/[coursename]/index.vue
@@ -9,11 +9,13 @@ if (typeof route.params.coursename !== "string") {
 
 const { data: materials } = await useFetch(`/api/course/${route.params.coursename}/materials`)
 const modalOpen = ref<boolean>(false);
+
+
 </script>
 <template>
   <div v-if="materials">
     <h2>materials</h2>
-    <button @click="modalOpen = true">Add Material</button>
+    <button @click="modalOpen = true">Add materials</button>
     <div v-if="materials.folderStructure.length !== 0">
       <ListItems :fileStructure="materials.folderStructure" />
     </div>
@@ -22,25 +24,6 @@ const modalOpen = ref<boolean>(false);
     </div>
     <div v-if="modalOpen"
       class="bg-black bg-opacity-30 absolute w-screen h-screen top-0 left-0 flex justify-center items-center">
-      <div class="bg-white ">
-        <div class="max-w-4xl">
-          <div>
-            <button>Folder</button>
-          </div>
-          <div>
-            <NuxtLink :to="route.path + '/quiz-create'">Quiz</NuxtLink>
-          </div>
-          <div>
-            <NuxtLink :to="route.path + '/assignment-create'">Assignment</NuxtLink>
-          </div>
-          <div>
-            <NuxtLink :to="route.path + '/page-create'">Page</NuxtLink>
-          </div>
-          <div>
-            <NuxtLink :to="route.path + '/forum-create'">Forums</NuxtLink>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
