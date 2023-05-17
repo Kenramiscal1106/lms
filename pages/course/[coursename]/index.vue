dbba5d9b60cbaf9e1552505ed7044a5e39f9f803
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-18 07:11:59 +0800

    refactor(pages): :art: migrate all logic to separate component

--- pages/course/[coursename]/index.vue
+++ pages/course/[coursename]/index.vue
@@ -7,24 +7,11 @@ if (typeof route.params.coursename !== "string") {
   })
 }
 
-const { data: materials } = await useFetch(`/api/course/${route.params.coursename}/materials`)
-const modalOpen = ref<boolean>(false);
-
 
 </script>
 <template>
-  <div v-if="materials">
-    <h2>materials</h2>
-    <button @click="modalOpen = true">Add materials</button>
-    <div v-if="materials.folderStructure.length !== 0">
-      <ListItems :fileStructure="materials.folderStructure" />
-    </div>
-    <div v-else>
-      There are no materials
-    </div>
-    <div v-if="modalOpen"
-      class="bg-black bg-opacity-30 absolute w-screen h-screen top-0 left-0 flex justify-center items-center">
-    </div>
-  </div>
+  <h2>materials</h2>
+  <FileStructure :route="route" />
 </template>
+
 <style scoped></style>
