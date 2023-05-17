5bef3cd4dc80c23b5720d784b71ca541c94ce70b
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-17 22:20:48 +0800

    refactor(pages): add div wrapper

--- pages/course/[coursename]/members.vue
+++ pages/course/[coursename]/members.vue
@@ -4,20 +4,22 @@ const { data } = useFetch(`/api/course/${route.params.coursename}/members`)
 // console.log(data.value.members)
 </script>
 <template>
-  <h2>Members</h2>
-  <table>
-    <tbody>
-      <tr>
-        <th>name</th>
-        <th>username</th>
-      </tr>
-      <template v-if="data">
-        <tr v-for="member in data.members">
-          <td>{{ member.firstName + " " + member.lastName }}</td>
-          <td>{{ member.username }}</td>
+  <div>
+    <h2>Members</h2>
+    <table>
+      <tbody>
+        <tr>
+          <th>name</th>
+          <th>username</th>
         </tr>
-      </template>
-    </tbody>
-  </table>
+        <template v-if="data">
+          <tr v-for="member in data.members">
+            <td>{{ member.firstName + " " + member.lastName }}</td>
+            <td>{{ member.username }}</td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+  </div>
 </template>
 <style scoped></style>
