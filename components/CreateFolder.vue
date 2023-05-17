3df318854fde570768b7550f05f966bf1736ab67
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-17 22:14:27 +0800

    feat(components): :sparkles: add components for modal

---
+++ components/CreateFolder.vue
@@ -0,0 +1,30 @@
+<script setup lang='ts'>
+const { route } = defineProps<{ route: ReturnType<typeof useRoute> }>()
+const { label } = defineProps<{ label: string }>()
+const addMaterial = (async (e) => {
+  const formTarget = e.currentTarget as HTMLFormElement;
+  const formData = Object.fromEntries(new FormData(formTarget).entries())
+  const postReq = await fetch(`/api/course/${route.params.coursename}/add-folder`, {
+    method: "POST",
+    body: JSON.stringify(formData)
+  })
+
+  if (!postReq.ok) {
+    console.log("bad request :(")
+    return
+  }
+  console.log("submitted successfully")
+}) satisfies EventListener
+
+</script>
+<template>
+  <div class="bg-white min-w-[300px]">
+    <form @submit.prevent="addMaterial">
+      <label for="material-name">Folder name</label>
+      <input type="text" name="name" id="material-name">
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+<style scoped></style>
---
+++ components/CreateMaterial.vue
@@ -0,0 +1,37 @@
+<script setup lang='ts'>
+const { route } = defineProps<{ route: ReturnType<typeof useRoute> }>()
+const { label } = defineProps<{ label: string }>()
+const addMaterial = (async (e) => {
+  const formTarget = e.currentTarget as HTMLFormElement;
+  const formData = Object.fromEntries(new FormData(formTarget).entries())
+  const postReq = await fetch(`/api/course/${route.params.coursename}/add-material`, {
+    method: "POST",
+    body: JSON.stringify(formData)
+  })
+
+  if (!postReq.ok) {
+    console.log("bad request :(")
+    return
+  }
+  console.log("submitted successfully")
+}) satisfies EventListener
+
+</script>
+<template>
+  <div class="bg-white min-w-[300px]">
+    <form @submit.prevent="addMaterial">
+      <label for="material-type">Material type</label>
+      <select name="type" id="material-type">
+        <option value="quiz">Quiz</option>
+        <option value="assignment">Assignment</option>
+        <option value="page">Page</option>
+        <option value="forums">Forums</option>
+      </select> <br />
+      <label for="material-name">Material name</label>
+      <input type="text" name="name" id="material-name">
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+<style scoped></style>
