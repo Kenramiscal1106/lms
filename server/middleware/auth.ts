4860b73bc9a69d7b6f3ebf8dd8690f042f72d454
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-25 03:14:47 +0800

    fix(api): :bug: fix bug by adding &&

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,7 +1,13 @@
 export default defineEventHandler((event) => {
   const sessionCookie = getCookie(event, "dbSession");
-  console.log(event.path);
-  if (!sessionCookie) {
+  // console.log(event.path);
+  if (
+    !sessionCookie &&
+    event.path !== "/favicon.ico" &&
+    event.path.startsWith("/api") &&
+    event.path !== "/api/login" &&
+    event.path !== "/api/register"
+  ) {
     throw createError({
       statusCode: 400,
       message: "unauthorized access",
