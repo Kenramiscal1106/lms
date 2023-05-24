2028c92254e2fce4356320c5d7dac2c7e1ed6d6d
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-25 03:46:14 +0800

    refactor(utilities): :alien:

--- utils/models.ts
+++ utils/models.ts
@@ -6,15 +6,9 @@ const { NUXT_MONGO_URL } = process.env;
 if (typeof NUXT_MONGO_URL === "undefined") {
   throw new Error("NUXT_MONGO_URL is not defined in the .env file");
 }
-mongoose.connect(
-  NUXT_MONGO_URL,
-  {
-    dbName: "lms",
-  },
-  () => {
-    console.log("connected to database");
-  }
-);
+mongoose.connect(NUXT_MONGO_URL, {
+  dbName: "lms",
+});
 // connectToDB();
 mongoose.set("strictQuery", false);
 const courses = new mongoose.Schema<CourseSchema>({
