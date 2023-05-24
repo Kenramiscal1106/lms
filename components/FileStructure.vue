632ba20bc48919eb6891df543e7d1875fb6fec32
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-24 17:27:41 +0800

    feat(components): :sparkles: add feature
    *add and edit actions for material and folder, rename route, added component if-else

--- components/FileStructure.vue
+++ components/FileStructure.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 const { route } = defineProps<{ route: ReturnType<typeof useRoute> }>()
-const { data: materials } = await useFetch(`/api/course/${route.params.coursename}/materials`);
+const { data: materials } = await useFetch(`/api/course/${route.params.courseid}/materials`);
 const modal = reactive<{
   type: "material" | "folder",
   open: boolean
@@ -11,11 +11,14 @@ const modal = reactive<{
 </script>
 
 <template>
-  <button @click="modal.open = true">Add materials</button>
+  <button @click="modal.open = true; modal.type = 'material'">Add materials</button>
+  <button @click="modal.open = true; modal.type = 'folder'">Add folder</button>
 
   <ListItems v-if="materials && materials.folderStructure.length !== 0" :fileStructure="materials.folderStructure" />
   <div v-show="modal.open"
     class="bg-black bg-opacity-30 absolute w-screen h-screen top-0 left-0 flex justify-center items-center">
+    <button @click="modal.open = false">Close modal</button>
     <CreateMaterial v-if="modal.type === 'material'" :route="route" />
+    <CreateFolder v-if="modal.type === 'folder'" :route="route" />
   </div>
 </template>
