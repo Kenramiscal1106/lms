7aaf61d32218f121ee234fe54bc7147bddcc9338
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-25 15:21:03 +0800

    feat(pages): :sparkles: add logic for fetching data on a page

--- pages/course/[courseid]/page/[pageid].vue
+++ pages/course/[courseid]/page/[pageid].vue
@@ -1,5 +1,19 @@
 <script setup lang='ts'>
+definePageMeta({
+  middleware: ["auth"]
+})
 
+const route = useRoute()
+const { data } = await useFetch(`/api/course/${route.params.courseid}/page/${route.params.pageid}`)
+if (!data.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Not found"
+  })
+}
 </script>
-<template></template>
+<template>
+  <h2>{{ data?.name }}</h2>
+  <p>{{ data?.text }}</p>
+</template>
 <style scoped></style>
