d90a78498c5be5dc233f4e031dd1451c1de29900
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-05-30 15:49:02 +0800

    feat(api): :sparkles: add endpoint for fetching posts

---
+++ server/api/course/[courseid]/posts.get.ts
@@ -0,0 +1,43 @@
+import type { QueryOptions } from "mongoose";
+import { Courses } from "~/utils/models";
+import { Post, UserSchema } from "~/utils/types";
+
+export default defineEventHandler(async (event) => {
+  const { limit, skip } = getQuery(event);
+  let options: QueryOptions = {};
+  if (skip && typeof skip === "string") {
+    options.skip = parseInt(skip);
+  } else if (typeof skip === "number") {
+    options.skip = skip;
+  }
+  if (limit && typeof limit === "string") {
+    options.limit = parseInt(limit);
+  } else if (typeof limit === "number") {
+    options.limit = limit;
+  }
+  const targetCourse = await Courses.findById(event.context.params?.courseid, {
+    posts: true,
+  }).populate<{
+    posts: (Omit<Post, "author"> & {
+      author: Pick<UserSchema, "_id" | "firstName" | "lastName" | "username">;
+    })[];
+  }>({
+    path: "posts",
+    populate: {
+      path: "author",
+      select: {
+        firstName: true,
+        lastName: true,
+        username: true,
+      },
+    },
+    options,
+  });
+  if (!targetCourse) {
+    throw createError({
+      statusCode: 404,
+      message: "not found",
+    });
+  }
+  return { posts: targetCourse.posts };
+});
