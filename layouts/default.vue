60889a8a4a102f2005ea8bca5ab98c6104015791
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-06-01 07:25:41 +0800

    feat(layouts): :sparkles: add loading indicator to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,9 @@
+<script lang="ts" setup>
+</script>
 <template>
+  <NuxtLoadingIndicator :height="5" :color="'hsl(190, 100%, 50%)'" />
   <Navbar />
   <slot></slot>
 </template>
 
-<style>
-@tailwind base;
-
-@layer base {
-  * {
-    font-family: "Inter";
-    border: 1px solid black;
-  }
-
-  h1 {
-    @apply text-3xl;
-  }
-
-  h2 {
-    @apply text-2xl;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    @apply font-bold;
-  }
-
-  ul {
-    @apply list-disc;
-  }
-}
-
-body {
-  margin-top: 5rem;
-}
-</style>
+<style></style>
