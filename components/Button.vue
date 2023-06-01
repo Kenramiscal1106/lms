b9f5dbf8363d67fe4127ffa36abeb83de081bc86
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-06-02 07:37:21 +0800

    feat(components): :sparkles: add button

---
+++ components/Button.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+const { variant } = defineProps<{
+  variant: "fill" | "outline"
+}>()
+</script>
+<template>
+  <button
+    :class="[variant === 'fill' ? 'bg-neutral-700 hover:bg-neutral-900 text-white' : 'border-2 border-neutral-300 hover:bg-neutral-300', 'px-3 py-2 rounded-md']">
+    <slot></slot>
+  </button>
+</template>
