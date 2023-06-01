6a6098ddfa2be874d2a307a58978e71b52451106
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-06-01 20:53:28 +0800

    feat(middleware): :sparkles: add next link after login redirect

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,5 +2,12 @@ import { useUserSession } from "~~/composables/authUser";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const user = await useUserSession();
-  if (!user.isLoggedIn) return navigateTo("/login");
+  if (!user.isLoggedIn) {
+    return navigateTo({
+      path: "/login",
+      query: {
+        next: encodeURIComponent(from.path),
+      },
+    });
+  }
 });
