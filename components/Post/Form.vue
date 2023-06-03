b8c730d46f52441e092536ffb0a9652ebc46029e
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-06-03 21:58:23 +0800

    feat(components): :sparkles: extract form logic of update page to a PostForm component

---
+++ components/Post/Form.vue
@@ -0,0 +1,24 @@
+<script setup lang='ts'>
+const route = useRoute();
+const formAction = (async (e) => {
+  const formTarget = e.currentTarget as HTMLFormElement;
+  const formData = Object.fromEntries(new FormData(formTarget).entries())
+  await $fetch(`/api/course/${route.params.courseid}/add-post`, {
+    method: 'POST',
+    body: JSON.stringify(formData),
+    async onResponse({ response }) {
+      if (response.ok) {
+        console.log("posts updated successfully");
+        await refreshNuxtData("posts")
+      }
+    }
+  })
+}) satisfies EventListener
+</script>
+<template>
+  <form @submit.prevent="formAction">
+    <textarea name="content"></textarea>
+    <Button type="submit" :variant='"fill"'>Submit</Button>
+  </form>
+</template>
+<style scoped></style>
