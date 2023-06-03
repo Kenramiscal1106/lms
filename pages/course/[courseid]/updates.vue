0a2bf0599d969ac133ed2b9d27c3b52f286cb6a8
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-06-03 22:02:05 +0800

    feat(pages): :sparkles: add data handling cases

--- pages/course/[courseid]/updates.vue
+++ pages/course/[courseid]/updates.vue
@@ -1,11 +1,23 @@
 <script setup lang='ts'>
 const route = useRoute()
-const { data } = await useFetch(`/api/course/${route.params.courseid}/posts`)
+const { data, pending, error } = await useLazyFetch(`/api/course/${route.params.courseid}/posts`, {
+  key: "posts",
+  server: false
+})
 </script>
 <template>
-  <div v-if="data">
+  <PostForm />
+  <div v-if="pending">
+    <h4>Loading...</h4>
+  </div>
+  <div v-else-if="error && !data">
+    <h4>
+      {{ error.message }}
+    </h4>
+  </div>
+  <div v-else-if="data">
     <div v-for="post in data.posts">
-      <h4>{{ post.author.username }}</h4>
+      <h4>{{ post.author.firstName + post.author.lastName }}</h4>
       <div>{{ post.content }}</div>
     </div>
   </div>
