d1e5ea4421dc72af33d541f8e75f3928de38cbdc
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-06-05 07:35:50 +0800

    feat(components): :sparkles: add post component

---
+++ components/Post/index.vue
@@ -0,0 +1,24 @@
+<script setup lang='ts'>
+import { Post, UserSchema } from '~/utils/types';
+
+const { post } = defineProps<{
+  post: Omit<Post, "author"> & {
+    author: Pick<UserSchema, "_id" | "firstName" | "lastName" | "username">;
+  }
+}>()
+console.log(post.createdAt)
+</script>
+<template>
+  <article>
+    <div>
+      <div>
+        {{ post.author.firstName + post.author.lastName }}
+      </div>
+      <div>
+        {{ post.createdAt }}
+      </div>
+    </div>
+    <div>{{ post.content }}</div>
+  </article>
+</template>
+<style scoped></style>
