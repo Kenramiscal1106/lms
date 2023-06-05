2949fe54089062fffbd60281e1b451d575f7670f
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-06-06 02:09:33 +0800

    refactor(api): add explicit return type

--- server/api/course/[courseid]/add-folder.post.ts
+++ server/api/course/[courseid]/add-folder.post.ts
@@ -1,42 +1,54 @@
 import { Users, Courses } from "~/utils/models";
 
-export default defineEventHandler(async (event) => {
-  // we want the routes to only be accessed by the teacher
-  const targetCourse = await Courses.findById(event.context.params?.courseid, {
-    members: true,
-  });
-  if (!targetCourse) {
-    throw createError({
-      statusCode: 400,
-      message: "user is null/targetCourse not found",
-    });
-  }
+export default defineEventHandler(
+  async (
+    event
+  ): Promise<{
+    success: boolean;
+    message: string;
+    type: string;
+  }> => {
+    // we want the routes to only be accessed by the teacher
+    const targetCourse = await Courses.findById(
+      event.context.params?.courseid,
+      {
+        members: true,
+      }
+    );
+    if (!targetCourse) {
+      throw createError({
+        statusCode: 400,
+        message: "user is null/targetCourse not found",
+      });
+    }
 
-  if (
-    typeof targetCourse.members.find(
-      (member) => member.toString() === event.context.userId.toString()
-    ) === "undefined"
-  ) {
-    throw createError({
-      statusCode: 400,
-      message: "you're not a member",
-    });
-  }
+    if (
+      typeof targetCourse.members.find(
+        (member) => member.toString() === event.context.userId.toString()
+      ) === "undefined"
+    ) {
+      throw createError({
+        statusCode: 400,
+        message: "you're not a member",
+      });
+    }
 
-  const body = await readBody(event);
-  Promise.allSettled([
-    targetCourse.updateOne({
-      $push: {
-        folderStructure: {
-          name: body.name,
-          type: "folder",
-          children: [],
+    const body = await readBody(event);
+    Promise.allSettled([
+      targetCourse.updateOne({
+        $push: {
+          folderStructure: {
+            name: body.name,
+            type: "folder",
+            children: [],
+          },
         },
-      },
-    }),
-  ]);
-  return {
-    success: true,
-    message: "updated successfully",
-  };
-});
+      }),
+    ]);
+    return {
+      success: true,
+      type: "success",
+      message: "updated successfully",
+    };
+  }
+);
--- server/api/course/[courseid]/add-post.post.ts
+++ server/api/course/[courseid]/add-post.post.ts
@@ -2,33 +2,48 @@
 import mongoose from "mongoose";
 import { Courses, Posts } from "~/utils/models";
 
-export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
+export default defineEventHandler(
+  async (
+    event
+  ): Promise<{
+    success: boolean;
+    message: string;
+    type: string;
+  }> => {
+    const body = await readBody(event);
 
-  const post = new Posts({
-    author: event.context.userId,
-    content: body.content,
-    comments: [],
-    course: new mongoose.Types.ObjectId(event.context.params?.courseid),
-    isComment: false,
-  });
-  try {
-    Promise.allSettled([
-      Courses.findByIdAndUpdate(event.context.params?.courseid, {
-        $push: {
-          posts: post._id,
-        },
-      }).exec(),
-
-      post.save(),
-    ]);
-  } catch {
-    throw createError({
-      statusCode: 500,
-      message: "unable to write to database",
+    const post = new Posts({
+      author: event.context.userId,
+      content: body.content,
+      comments: [],
+      course: new mongoose.Types.ObjectId(event.context.params?.courseid),
+      isComment: false,
     });
+    try {
+      Promise.allSettled([
+        Courses.findByIdAndUpdate(event.context.params?.courseid, {
+          $push: {
+            posts: post._id,
+          },
+        }).exec(),
+
+        post.save(),
+      ]);
+    } catch {
+      throw createError({
+        statusCode: 500,
+        message: "unable to write to database",
+        data: {
+          success: false,
+          type: "error",
+          message: "unable to write to database (status: 500)",
+        },
+      });
+    }
+    return {
+      success: true,
+      type: "error",
+      message: "uploaded post",
+    };
   }
-  return {
-    success: true,
-  };
-});
+);
