c7139cd1be7bcbb08a14830826f792fcad9bc71a
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-06-06 02:10:17 +0800

    update style issue

--- components/Course/SidebarNav.vue
+++ components/Course/SidebarNav.vue
@@ -7,7 +7,7 @@ const { to } = defineProps<{
 </script>
 <template>
   <NuxtLink :to="`/course/${route.params.courseid}${to === 'materials' ? '' : '/' + to}`"
-    :class="[`px-3 py-2 no-underline text-black border-l-4`, route.path.endsWith(to === 'materials' ? '' : to) ? '' : 'hover:bg-neutral-200 border-l-transparent']"
+    :class="[`px-3 py-2 no-underline text-black border-l-4`, route.path.endsWith(to === 'materials' ? route.params.courseid as string : to) ? '' : 'hover:bg-neutral-200 border-l-transparent']"
     :exact-active-class="'font-bold bg-neutral-300 hover:bg-neutral-300 border-blue-500'">
     <slot></slot>
   </NuxtLink>
