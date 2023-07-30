0a15d6d366e7875432503a10c3f5c890258dd81c
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-07-31 06:33:58 +0800

    refactor(components): :art: add placeholder; update formatting

--- components/Post/Form.vue
+++ components/Post/Form.vue
@@ -1,24 +1,24 @@
-<script setup lang='ts'>
+<script setup lang="ts">
 const route = useRoute();
 const formAction = (async (e) => {
   const formTarget = e.currentTarget as HTMLFormElement;
-  const formData = Object.fromEntries(new FormData(formTarget).entries())
+  const formData = Object.fromEntries(new FormData(formTarget).entries());
   const result = await $fetch(`/api/course/${route.params.courseid}/add-post`, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify(formData),
     async onResponse({ response }) {
       if (response.ok) {
         console.log("posts updated successfully");
-        await refreshNuxtData("posts")
+        await refreshNuxtData("posts");
       }
-    }
-  })
-}) satisfies EventListener
+    },
+  });
+}) satisfies EventListener;
 </script>
 <template>
   <form @submit.prevent="formAction">
-    <textarea name="content"></textarea>
-    <Button type="submit" :variant='"fill"'>Submit</Button>
+    <textarea name="content" placeholder="Say something..."></textarea>
+    <Button type="submit" :variant="'fill'">Submit</Button>
   </form>
 </template>
 <style scoped></style>
